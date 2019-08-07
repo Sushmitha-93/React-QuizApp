8c93569b72c1ae45ff041cc80b8ea392453c15e0
Sushmitha DT <dtsushmitha@gmail.com>
2019-08-08 02:29:04 +0530

    onClick functionality

--- src/App.js
+++ src/App.js
@@ -11,36 +11,53 @@ class App extends Component {
     answer: 0,
     totalQues: data.length,
     score: 0,
-    qn: 0 //current question number
+    qn: -1 //current question index
   };
 
-  setNextQuesData() {
-    const { qn } = this.state;
+  setNextQuesData = () => {
+    let { qn } = this.state;
+    qn++;
     this.setState({
       question: data[qn].question,
       options: [...data[qn].options],
       answer: data[qn].answer,
-      qn: qn + 1
+      qn: qn
     });
-  }
+  };
 
   componentDidMount() {
     this.setNextQuesData(this.state.qn);
   }
 
-  increaseScore() {
+  increaseScore = () => {
     this.setState({ score: this.state.score + 1 });
-  }
+    console.log("Score:", this.state.score);
+  };
 
-  handleOnClick(option) {
+  checkAnswer = option => {
+    const { qn } = this.state;
+    return option === data[qn].answer ? true : false;
+  };
+
+  handleOnClick = option => {
     console.log(option);
-  }
+
+    const answer = this.checkAnswer(option);
+    console.log(answer);
+
+    if (this.state.qn + 1 < this.state.totalQues) {
+      if (answer) {
+        this.increaseScore();
+      }
+      this.setNextQuesData();
+    }
+  };
   render() {
     return (
       <div className="container">
         <div className="card col-md-10 offset-md-1 vertical-center">
           <h5 className="card-header">
-            {this.state.qn}. {this.state.question}
+            {this.state.qn + 1}. {this.state.question}
           </h5>
           <Options options={this.state.options} onClick={this.handleOnClick} />
         </div>
