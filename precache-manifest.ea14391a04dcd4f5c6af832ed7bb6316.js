7d057a1652d753339c08c9ed919a5ab793cf0f23
Sushmitha DT <dtsushmitha@gmail.com>
2019-08-12 21:19:08 +0530

    Updates

---
+++ precache-manifest.ea14391a04dcd4f5c6af832ed7bb6316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74728cd876d1539c503e921a2856044",
+    "url": "/React-QuizApp/index.html"
+  },
+  {
+    "revision": "aad5b2bec99d3328b00c",
+    "url": "/React-QuizApp/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f67d9ffbac25e0fc657d",
+    "url": "/React-QuizApp/static/css/main.af1e1529.chunk.css"
+  },
+  {
+    "revision": "aad5b2bec99d3328b00c",
+    "url": "/React-QuizApp/static/js/2.a96534e6.chunk.js"
+  },
+  {
+    "revision": "f67d9ffbac25e0fc657d",
+    "url": "/React-QuizApp/static/js/main.4a48a40a.chunk.js"
+  },
+  {
+    "revision": "0844ce6d8b98b81a7fd9",
+    "url": "/React-QuizApp/static/js/runtime~main.b87e0bbb.js"
+  }
+]);
